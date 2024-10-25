31b2a7afde4c7ae0b9977d1c2026181da5816b3a
Marvelamin <git@stackblitz.com>
2024-10-25 17:23:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sb1-85tmkhr
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Marvelamin/sb1-85tmkhr)
---
+++ src/store/authStore.ts
@@ -0,0 +1,38 @@
+import { create } from 'zustand';
+import { AuthState, User } from '../types';
+
+const initialUsers: User[] = [
+  {
+    id: '1',
+    username: 'admin',
+    password: 'admin123',
+    role: 'admin',
+  },
+];
+
+export const useAuthStore = create<AuthState>((set, get) => ({
+  user: null,
+  users: initialUsers,
+  login: (username: string, password: string) => {
+    const user = get().users.find(
+      (u) => u.username === username && u.password === password
+    );
+    if (user) {
+      set({ user });
+      return true;
+    }
+    return false;
+  },
+  logout: () => set({ user: null }),
+  addUser: (newUser: Omit<User, 'id'>) => {
+    const id = Math.random().toString(36).substr(2, 9);
+    set((state) => ({
+      users: [...state.users, { ...newUser, id }],
+    }));
+  },
+  removeUser: (id: string) => {
+    set((state) => ({
+      users: state.users.filter((user) => user.id !== id),
+    }));
+  },
+}));
